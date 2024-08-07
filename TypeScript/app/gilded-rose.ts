5e1801f94d978e89b2b1f7a4bbedc6e10061b264
SamVanHoof <samvanhoof@hotmail.com>
2024-08-07 17:19:39 +0200

    Add constants and utils

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,5 @@
+import { qualityHandlers } from "./utils/qualityHandlers";
+
 export class Item {
   name: string;
   sellIn: number;
@@ -17,37 +19,6 @@ export class GildedRose {
     this.items = items;
   }
 
-  private MAX_QUALITY = 50;
-  private MIN_QUALITY = 0;
-  private MAX_QUALITY_LEGENDARY = 80;
-
-  private improveItem = (item: Item, amount: number, isLegendary?: boolean) => {
-    const maxQuality = isLegendary
-      ? this.MAX_QUALITY_LEGENDARY
-      : this.MAX_QUALITY;
-
-    if (item.quality + amount >= maxQuality) item.quality = maxQuality;
-    else item.quality += amount;
-  };
-
-  private deteriorateItem = (item: Item, amount: number) => {
-    if (item.sellIn < 0) amount = amount * 2;
-    if (item.quality - amount < this.MIN_QUALITY)
-      item.quality = this.MIN_QUALITY;
-    else item.quality -= amount;
-  };
-
-  private handleAgingItem = (item: Item) => {
-    this.improveItem(item, 1);
-  };
-
-  private handleTimeSensitiveItem = (item: Item) => {
-    if (item.sellIn <= 0) item.quality = 0;
-    else if (item.sellIn < 5) this.improveItem(item, 3);
-    else if (item.sellIn < 10) this.improveItem(item, 2);
-    else this.improveItem(item, 1);
-  };
-
   updateQuality() {
     for (let item of this.items) {
       console.log(item.name);
@@ -55,21 +26,21 @@ export class GildedRose {
       switch (item.name) {
         case "Aged Brie":
           item.sellIn -= 1;
-          this.handleAgingItem(item);
+          qualityHandlers.handleAgingItem(item);
           break;
         case "Backstage passes to a TAFKAL80ETC concert":
           item.sellIn -= 1;
-          this.handleTimeSensitiveItem(item);
+          qualityHandlers.handleTimeSensitiveItem(item);
           break;
         case "Conjured Mana Cake":
           item.sellIn -= 1;
-          this.deteriorateItem(item, 2);
+          qualityHandlers.deteriorateItem(item, 2);
           break;
         case "Sulfuras, Hand of Ragnaros":
           break;
         default:
           item.sellIn -= 1;
-          this.deteriorateItem(item, 1);
+          qualityHandlers.deteriorateItem(item, 1);
           break;
       }
     }
---
+++ TypeScript/app/utils/qualityHandlers.ts
@@ -0,0 +1,32 @@
+import {
+  MAX_QUALITY,
+  MAX_QUALITY_LEGENDARY,
+  MIN_QUALITY,
+} from "@/constants/qualityValues";
+import { Item } from "@/gilded-rose";
+
+export class qualityHandlers {
+  static improveItem = (item: Item, amount: number, isLegendary?: boolean) => {
+    const maxQuality = isLegendary ? MAX_QUALITY_LEGENDARY : MAX_QUALITY;
+
+    if (item.quality + amount >= maxQuality) item.quality = maxQuality;
+    else item.quality += amount;
+  };
+
+  static deteriorateItem = (item: Item, amount: number) => {
+    if (item.sellIn < 0) amount = amount * 2;
+    if (item.quality - amount < MIN_QUALITY) item.quality = MIN_QUALITY;
+    else item.quality -= amount;
+  };
+
+  static handleAgingItem = (item: Item) => {
+    qualityHandlers.improveItem(item, 1);
+  };
+
+  static handleTimeSensitiveItem = (item: Item) => {
+    if (item.sellIn <= 0) item.quality = 0;
+    else if (item.sellIn < 5) qualityHandlers.improveItem(item, 3);
+    else if (item.sellIn < 10) qualityHandlers.improveItem(item, 2);
+    else qualityHandlers.improveItem(item, 1);
+  };
+}
